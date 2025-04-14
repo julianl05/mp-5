53cc86f9bf341b9f5f2b862ef47d5a2720d5a558
Julian Lim Jun Ren <limjunre@bu.edu>
2025-04-14 14:02:10 -0400

    added url validation for reachability

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,7 @@ export default function Home() {
     }
     const res = await createNewUrl(url, alias);
     setResult(res);
+    console.log(res);
   };
   
   return (
--- lib/storeUrl.ts
+++ lib/storeUrl.ts
@@ -7,7 +7,21 @@ export default async function storeUrl(url: string, alias: string): Promise<stri
     try {
         new URL(url);
     } catch (e) {
-        return "Error: Invalid URL";
+        return "Error: Invalid URL, format incorrect";
+    }
+    const urlRes = await fetch(url, { method: "HEAD" })
+        .then((res) => {
+            console.log("Response: ", res.status);
+            if (res.status < 200 || res.status > 500) {
+                return "Error";
+            }
+        })
+        .catch((e) => {
+            console.log("Error: ", e);
+            return "Error";
+        })
+    if (urlRes === "Error") {
+        return "Error: URL not reachable";
     }
     const shortUrl = {
         alias: alias,
@@ -16,7 +30,6 @@ export default async function storeUrl(url: string, alias: string): Promise<stri
     const urlCollection = await getCollection(URL_COLLECTION);
     const existingUrl = await urlCollection.findOne({ alias: shortUrl.alias });
     if (existingUrl) {
-        alert("Alias already exists");
         return "Error: Alias already exists";
     }
     const res = await urlCollection.insertOne({...shortUrl});
