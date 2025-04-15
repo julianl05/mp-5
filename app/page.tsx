c54cab79d56c37426f243f4b4d7956598ab90af3
Julian Lim Jun Ren <limjunre@bu.edu>
2025-04-14 21:08:26 -0400

    made some adjustments to styling

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
   
   return (
     <div>
-      <form onSubmit={handleClick} className="flex flex-col rounded-3xl w-[40vw] my-[15vh] px-[2vw] py-[5vh] justify-self-center bg-[#31363F] shadow-lg">
+      <form onSubmit={handleClick} className="flex flex-col rounded-3xl w-[60vw] my-[15vh] px-[2vw] py-[5vh] justify-self-center bg-[#31363F] shadow-lg">
         <h1 className="text-[calc(3px+2vw)] w-full text-center">URL Shortener</h1>
         <div className="flex flex-col w-full">
           <label htmlFor="url-input" className="mt-[2vh]">URL</label> 
@@ -38,8 +38,8 @@ export default function Home() {
           />
         </div>
         <div className="flex flex-col w-full">
-          <label htmlFor="alias-input" className="mt-[1vh]">Alias</label> 
-          <div className="flex flex-row items-center pb-[1vw]">
+          <label htmlFor="alias-input" className="mt-[1vw]">Alias</label> 
+          <div className="flex flex-row items-center pb-[1vw] flex-wrap"> 
             <p className="text-gray-100">https://mp-5-gules.vercel.app/</p>
             <input 
               id="alias-input"
@@ -47,13 +47,13 @@ export default function Home() {
               value={alias}
               placeholder="example"
               onChange={(e) => setAlias(e.target.value)}
-              className="border-2 border-gray-500 rounded-md p-[0.5vw] ml-[1vw] w-full text-gray-400"
+              className="flex-shrink flex-1 border-2 border-gray-500 rounded-md p-[0.5vw] ml-[1vw] text-gray-400" //flex-shrink should let input width shrink if there is less room and flex-1 should let it grow if there is more room
             />
           </div>
         </div>
-        <button type="submit" className="text-[calc(8px+1vw)] w-full bg-[#76ABAE] p-[1vw] rounded-2xl hover:bg-gray-500 hover:cursor-pointer active:bg-gray-600">Shorten</button>
+        <button type="submit" className="text-[calc(3px+1.5vw)] w-full bg-[#76ABAE] p-[1vw] rounded-2xl hover:bg-gray-500 hover:cursor-pointer active:bg-gray-600">Shorten</button>
         {error ? (
-            <p className="text-red-300 text-center mt-[2vh]">{error}</p> 
+            <p className="text-red-300 text-center mt-[2vw]">{error}</p> 
           ) : result ? (
             <div className="p-[1vw] my-[2vh] bg-[#383d47]">
               <p className="text-green-300 text-center">Success!</p>
