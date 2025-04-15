10677d77835e5881cf155198e646ab68ea5b3e5c
Julian Lim Jun Ren <limjunre@bu.edu>
2025-04-14 20:38:09 -0400

    back to awaiting params

--- app/[url]/page.tsx
+++ app/[url]/page.tsx
@@ -2,7 +2,7 @@ import {redirect} from "next/navigation";
 import getUrl from "@/lib/getUrl";
 export default async function Url({params}: { params: { url: string } }) {
     let destination: string;
-    const {url} =  params; // next.js docs say to do await for params as it is now a promise in v15 and above
+    const {url} =  await params; // next.js docs say to do await for params as it is now a promise in v15 and above
     try {
         destination = await getUrl(url);
     } catch (e) {
